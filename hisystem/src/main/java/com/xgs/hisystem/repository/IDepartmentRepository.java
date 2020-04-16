c6a85d230062a49b26e83a991650c6693a0d1d6d
luoshijun-xl <3529815040@qq.com>
2020-04-16 20:58:58 +0800

    hisystem file

---
+++ hisystem/src/main/java/com/xgs/hisystem/repository/IDepartmentRepository.java
@@ -0,0 +1,13 @@
+package com.xgs.hisystem.repository;
+
+import com.xgs.hisystem.pojo.entity.DepartmentEntity;
+
+/**
+ * @author XueGuiSheng
+ * @date 2020/3/23
+ * @description:
+ */
+public interface IDepartmentRepository extends BaseRepository<DepartmentEntity>{
+
+    DepartmentEntity findByNameAndAddress(String name,String address);
+}
